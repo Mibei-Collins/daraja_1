9de5054cfc1ad11e3fb2eda0d1ffdeb085d8ea1e
Mibei Collins <106762145+Mibei-Collins@users.noreply.github.com>
2023-04-22 07:43:13 +0300

    Added new updates

--- timeout.php
+++ timeout.php
@@ -1,24 +1,14 @@
 <?php
 
-# set the timeout duration in seconds
-$timeout_duration = 120;
+$ResultCode = 0;
+$ResultDesc = "The service was accepted for processing.";
 
-# get the request body
-$request_body = file_get_contents('php://input');
+$Response = array(
+    'ResultCode' => $ResultCode,
+    'ResultDesc' => $ResultDesc
+);
 
-# parse the request body to get the transaction status
-$request_data = json_decode($request_body, true);
-$status = $request_data['Result']['ResultCode'];
+$ResponseJSON = json_encode($Response);
 
-# if the transaction is still being processed, wait for the specified duration and then re-queue the request
-if ($status == 0) {
-  sleep($timeout_duration);
-  http_response_code(202);
-  exit();
-}
-
-# if the transaction has completed or failed, return the status to the caller
-http_response_code(200);
-echo json_encode($request_data);
-
-?>
+header('Content-Type: application/json');
+echo $ResponseJSON;
