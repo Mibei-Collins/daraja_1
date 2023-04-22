447fb0e54c6b6c9e8286fde7c37a3cc0f13aaea2
Mibei Collins <106762145+Mibei-Collins@users.noreply.github.com>
2023-04-22 07:25:56 +0300

    Added new updates

--- results.php
+++ results.php
@@ -1,19 +1,40 @@
 <?php
-if(isset($_POST['submit'])){
-  
-  // rest of the code...
-  
-  // Get response code and message from M-Pesa API response
-  $response_code = $curl_response['ResponseCode'];
-  $response_message = $curl_response['ResponseDescription'];
-  
-  // Check if payment was successful
-  if($response_code == '0'){
-    echo "Payment successful: " . $response_message;
-  }
-  else{
-    echo "Payment failed: " . $response_message;
-  }
-  
+// Retrieve the POST data sent by M-Pesa API
+$callbackJSONData = file_get_contents('php://input');
+
+// Log the M-Pesa response
+$logFile = "logs/response.log";
+$log = fopen($logFile, "a");
+fwrite($log, $callbackJSONData . PHP_EOL);
+fclose($log);
+
+// Decode the JSON data received
+$callbackData = json_decode($callbackJSONData);
+
+// Retrieve the result code and result description
+$resultCode = $callbackData->ResultCode;
+$resultDesc = $callbackData->ResultDesc;
+
+// If transaction was successful, perform necessary actions
+if ($resultCode == 0) {
+    // Retrieve the transaction ID and amount
+    $transID = $callbackData->TransactionID;
+    $amount = $callbackData->Amount;
+
+    // Perform necessary actions e.g. update database, notify user, etc.
+    // ...
+
+    // Send response back to M-Pesa API
+    header("Content-Type: application/json");
+    echo '{"ResultCode":0, "ResultDesc":"The service was accepted successfully"}';
+} else {
+    // If transaction failed, log the error and send response back to M-Pesa API
+    $errorLog = "logs/error.log";
+    $log = fopen($errorLog, "a");
+    fwrite($log, "Error: $resultCode - $resultDesc" . PHP_EOL);
+    fclose($log);
+
+    header("Content-Type: application/json");
+    echo '{"ResultCode":1, "ResultDesc":"The service was not accepted"}';
 }
 ?>
--- timeout.php
+++ timeout.php
@@ -1,20 +1,24 @@
 <?php
-// set the timeout duration in seconds
-$timeout_duration = 300;
 
-// get the start time
-$start_time = time();
+# set the timeout duration in seconds
+$timeout_duration = 120;
 
-// loop until the timeout duration has elapsed
-while ((time() - $start_time) < $timeout_duration) {
-    // check if the payment has been completed
-    // you can do this by checking the status of the payment in your database or in the MPESA API
-    // if the payment has been completed, you can exit the loop and return a success response
+# get the request body
+$request_body = file_get_contents('php://input');
 
-    // if the payment is still in progress, wait for a short time before checking again
-    sleep(1);
+# parse the request body to get the transaction status
+$request_data = json_decode($request_body, true);
+$status = $request_data['Result']['ResultCode'];
+
+# if the transaction is still being processed, wait for the specified duration and then re-queue the request
+if ($status == 0) {
+  sleep($timeout_duration);
+  http_response_code(202);
+  exit();
 }
 
-// if the timeout duration has elapsed, return a failure response
-echo "Payment timed out";
+# if the transaction has completed or failed, return the status to the caller
+http_response_code(200);
+echo json_encode($request_data);
+
 ?>
