0dfadbf69189f4e3902d5c6b1e7408c066d409b4
Mibei Collins <106762145+Mibei-Collins@users.noreply.github.com>
2023-04-23 18:54:57 +0300

    New updates

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,14 +5,14 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = '6u2l6BdioLH1kalyGrUWNXaS6qCCjUbV'; //Fill with your app Consumer Key
-  $consumerSecret = ' GGmKYfueo0wkBi8q'; // Fill with your app Secret
+  $consumerKey = 'fGdVMGO2Ecur8FTQbEoAZRNoA4lounSQ'; //Fill with your app Consumer Key
+  $consumerSecret = 'NVRWDukLSFUEXDE7'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
-  $InitiatorName = 'testapi';
+  $InitiatorName = 'COLLINS KIPKOSGEI';
   
-  $PartyA = '600991';
+  $PartyA = '6331806';
   
   
   
@@ -25,14 +25,14 @@ if(isset($_POST['submit'])){
   $CommandID = 'SalaryPayment';    
   
   # Security Credential
-  $SecurityCredential = 'SzPh+Krz5jVzJne1HM5LQsP44IYeuuji8z5PkjblZFEbTh55eC3yvfipjI/nCbSKh5mxq1fTYV1AVWrY+8l+5ra479Ax7IAEjMJs6IG764lfeKUb1N+UP3PK8eDYNpQWX5fENQTs7oujfsQDtDwDSyvrywgQsQKqQggewQ+Av74tYjkuH+2SDTexMzEcEJryNgXlYZQ8DABXV6umkumuEzJ/PiG/tdieWO2DAeHawVOKBmWQXZ74Z7XSg0hDP+7UQpPqFNAlvngxIFb5EgpJ9oJWJULBIx1JC1RCBlXR3Wc3fKz9Huldm/O1A7eeUdrGZvlJThV5TXgSVaVLAhXHIQ==';
+  $SecurityCredential = 'B4TNU43PY67qsI6o06+BzGOrvHTHdMi6QFgR9rtnejr5ZUUpnRioY3l3K7/uXrXZEifLRnoTVHHGQ5jMOnjSo5/Dd5ia24zP4PTf+cnZrJLosDCUUChVom0MC6MOS8e0tzPtm2mm+aAz/YYoC7JRGlFHbu8hc1AqrPvzrPSVcSIvLIDQDh89FONw8FHBw0tjwnFhePOw/H+MBDqr5HTPaEocWvDCgG3ificWaMAXAfr64mSHZLz9kKtE3P4Y4Z6nXpczXQuE3cmu7UvhiFd6w+pZMBzHGAKZuc5z85/oFmLCnbLwX+uvdtoDstTFJOx0dx38lQz76c/P+3yvccDhJA==';
 
   # header for access token
   $headers = ['Content-Type:application/json; charset=utf8'];
 
     # M-PESA endpoint urls
-  $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-  $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest';
+  $access_token_url = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+  $initiate_url = 'https://api.safaricom.co.ke/mpesa/b2c/v1/paymentrequest';
 
   # callback url
   $Results = 'https://evening-scrubland-40886.herokuapp.com/results.php';
