c3bf6fd10ba0ce4ee630d01d7ae227e652cfda86
Mibei Collins <106762145+Mibei-Collins@users.noreply.github.com>
2023-04-23 13:29:13 +0300

    New updates

--- stk_initiate.php
+++ stk_initiate.php
@@ -12,7 +12,7 @@ if(isset($_POST['submit'])){
   # provide the following details, this part is found on your test credentials on the developer account
   $InitiatorName = 'testapi';
   
-  $PartyA = '600992';
+  $PartyA = '6331806';
   
   
   
