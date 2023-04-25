575f77d5b9f053ced36582e2c4884efd5d604e58
Mibei Collins <106762145+Mibei-Collins@users.noreply.github.com>
2023-04-25 13:52:23 +0300

    New updates

--- stk_initiate.php
+++ stk_initiate.php
@@ -10,7 +10,7 @@ if(isset($_POST['submit'])){
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
-  $InitiatorName = 'CM';
+  $InitiatorName = 'Student';
   
   $PartyA = '6331806';
   
